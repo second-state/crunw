d0efcccde8e137c80fd9c9f5f48271d591b508bd
Michael Yuan <michael@secondstate.io>
2021-11-08 02:26:09 -0600

    Update running_wasm_in_kubernetes.sh
    Signed-off-by: Michael Yuan <michael@michaelyuan.com>

--- running_wasm_in_kubernetes.sh
+++ running_wasm_in_kubernetes.sh
@@ -10,12 +10,13 @@ source /home/${USER}/.profile
 echo -e "Defaults        secure_path=\"/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\"" | sudo tee -i /etc/sudoers.d/gofile
 echo -e "Cloning Kubernetes ..."
 git clone https://github.com/kubernetes/kubernetes.git
+cd kubernetes/
 git checkout v1.22.2
+cd ../
 echo -e "Installing etcd"
 sudo CGROUP_DRIVER=systemd CONTAINER_RUNTIME=remote CONTAINER_RUNTIME_ENDPOINT='unix:///var/run/crio/crio.sock' ./kubernetes/hack/install-etcd.sh
 export PATH="/home/${USER}/kubernetes/third_party/etcd:${PATH}"
 sudo cp -rp ./kubernetes/third_party/etcd/etcd* /usr/local/bin/
 echo -e "Building and running k8s with CRI-O"
 sudo apt-get install -y build-essential
-nohup sudo -b CGROUP_DRIVER=systemd CONTAINER_RUNTIME=remote CONTAINER_RUNTIME_ENDPOINT='unix:///var/run/crio/crio.sock' ./kubernetes/hack/local-up-cluster.sh &
-
+nohup sudo -b CGROUP_DRIVER=systemd CONTAINER_RUNTIME=remote CONTAINER_RUNTIME_ENDPOINT='unix:///var/run/crio/crio.sock' ./kubernetes/hack/local-up-cluster.sh
